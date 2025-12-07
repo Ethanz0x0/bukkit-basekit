c85a93afced1168dd13d33c77e6d31ef94713da7
Ethanz0x0 <ethanz0x0@outlook.com>
2025-12-07 18:14:43 +0800

    Debugs

--- src/main/java/io/github/ethanz0x0/basekit/BaseKit.java
+++ src/main/java/io/github/ethanz0x0/basekit/BaseKit.java
@@ -3,6 +3,8 @@ package io.github.ethanz0x0.basekit;
 import io.github.ethanz0x0.basekit.listeners.PlayerListeners;
 import io.github.ethanz0x0.basekit.utils.PlaceholderAPIHook;
 import io.github.ethanz0x0.basekit.utils.StartupInfo;
+import io.github.ethanz0x0.basekit.utils.VersionHelper;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BaseKit extends JavaPlugin {
@@ -13,19 +15,22 @@ public class BaseKit extends JavaPlugin {
         return instance;
     }
 
-    //
+
     @Override
     public void onEnable() {
         instance = this;
-        getLogger().info("BaseKit is loading...");
+        getLogger().info("-------------------- BaseKit --------------------");
+        getLogger().info("Minecraft version: ");
         StartupInfo.checkAndCorrect();
         PlaceholderAPIHook.checkAndHook();
+        System.out.println(Bukkit.getVersion());
+        System.out.println(Bukkit.getBukkitVersion());
         if (StartupInfo.isFirstStart()) {
             getLogger().info("-------------------------------------------------------");
             getLogger().info("It seems that you are launching the current " +
                     "version of the plugin for the first time, ");
             getLogger().info("please check the full changelog at " +
-                    "https://github.com/Ethanz0x0/bukkit-basekit/releases/");
+                    "https://github.com/Ethanz0x0/bukkit-basekit/releases/tag/" + VersionHelper.getMainVersion());
             getLogger().info("-------------------------------------------------------");
         }
         getServer().getPluginManager().registerEvents(new PlayerListeners(), this);
---
+++ src/main/java/io/github/ethanz0x0/basekit/utils/MinecraftVersion.java
@@ -0,0 +1,60 @@
+package io.github.ethanz0x0.basekit.utils;
+
+public enum MinecraftVersion {
+
+    MINECRAFT_1_18(757, "1.18"),
+    MINECRAFT_1_18_2(758, "1.18.2"),
+    MINECRAFT_1_19(759, "1.19"),
+    MINECRAFT_1_19_1(760, "1.19.1"),
+    MINECRAFT_1_19_3(761, "1.19.3"),
+    MINECRAFT_1_19_4(762, "1.19.4"),
+    MINECRAFT_1_20(763, "1.20"),
+    MINECRAFT_1_20_2(764, "1.20.2"),
+    MINECRAFT_1_20_3(765, "1.20.3"),
+    MINECRAFT_1_20_5(766, "1.20.5"),
+    MINECRAFT_1_21(767, "1.21"),
+    MINECRAFT_1_21_2(768, "1.21.2"),
+    MINECRAFT_1_21_4(769, "1.21.4"),
+    MINECRAFT_1_21_5(770, "1.21.5"),
+    MINECRAFT_1_21_6(771, "1.21.6"),
+    MINECRAFT_1_21_7(772, "1.21.7"),
+    MINECRAFT_1_21_9(773, "1.21.9");
+
+    private final int protocolVersion;
+    private final String minecraftVersion;
+
+    MinecraftVersion(int protocolVersion, String minecraftVersion) {
+        this.protocolVersion = protocolVersion;
+        this.minecraftVersion = minecraftVersion;
+    }
+
+    public int getProtocolVersion() {
+        return protocolVersion;
+    }
+
+    public String getMinecraftVersion() {
+        return minecraftVersion;
+    }
+
+    public boolean isAfter(int protocol) {
+        return this.protocolVersion > protocol;
+    }
+
+    public boolean isBefore(int protocol) {
+        return this.protocolVersion < protocol;
+    }
+
+    public static MinecraftVersion getByProtocol(int protocol) {
+        for (MinecraftVersion version : values()) {
+            if (version.getProtocolVersion() == protocol) {
+                return version;
+            }
+        }
+        return null;
+    }
+
+    public static String getVersionStringByProtocol(int protocol) {
+        MinecraftVersion version = getByProtocol(protocol);
+        return version != null ? version.getMinecraftVersion() : "Unknown";
+    }
+}
