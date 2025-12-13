75d5b2a93152b0ee9fe2f3868b07ce5eb664f682
Ethanz0x0 <ethanz0x0@outlook.com>
2025-12-13 23:09:17 +0800

    Fixed several problems An update test Fixed workflow probelems

--- src/main/java/io/github/ethanz0x0/basekit/commands/BaseKitCommand.java
+++ src/main/java/io/github/ethanz0x0/basekit/commands/BaseKitCommand.java
@@ -2,6 +2,7 @@ package io.github.ethanz0x0.basekit.commands;
 
 import io.github.ethanz0x0.basekit.config.Config;
 import io.github.ethanz0x0.basekit.config.Messages;
+import io.github.ethanz0x0.basekit.schedulers.UpdateCheckerScheduler;
 import io.github.ethanz0x0.basekit.utils.MessageUtil;
 import io.github.ethanz0x0.basekit.utils.UpdateChecker;
 import org.bukkit.command.Command;
@@ -29,6 +30,7 @@ public class BaseKitCommand extends Command {
                 case "version", "ver" -> {
                     MessageUtil.sendMessage(sender, Messages.getPrefixedMessage("command-check-update-checking"));
                     UpdateChecker.check().thenAccept(latest -> {
+                        UpdateCheckerScheduler.NEED_UPDATE = !latest;
                         if (latest) {
                             MessageUtil.sendMessage(sender, Messages.getPrefixedMessage("command-check-update-latest"));
                         } else {
@@ -43,6 +45,7 @@ public class BaseKitCommand extends Command {
                     Config.getMessagesConfig().reload();
                     Config.getStartupInfoConfig().reload();
                     MessageUtil.sendMessage(sender, Messages.getPrefixedMessage("command-reload-success"));
+                    return true;
                 }
             }
         }
